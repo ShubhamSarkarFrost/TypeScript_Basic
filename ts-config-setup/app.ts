5acab996bd0fedec969c27acc6c2f53229d820d5
Shubham Sarkar <49359694+ShubhamSarkarFrost@users.noreply.github.com>
2023-08-28 14:22:17 +0530

    TYPESCRIPT_28-08-2023_2:22_pm_ist

--- ts-config-setup/app.ts
+++ ts-config-setup/app.ts
@@ -1,1 +1,4 @@
-console.log('Time to get started...');
+// console.log('Time to get started...');
+
+// to Enter into Watch mode with a File use tsc app.ts --watch or tsc app.ts -w
+// to create a watch mode for list of files use ts --init , it will generate tsconfig.json file 
