f60d56eea7804aae360d1d9e9801f033b53f0ade
Ronnie <roiecse8@gmail.com>
2023-09-03 09:43:56 -0700

    TYPRESCRIPT_DEV_MODULE_7_03_09_2023_09:43_PM_IST

---
+++ tsc-main-mega-project-drag_drop/src/app.ts
@@ -0,0 +1,11 @@
+// Code goes here!
+class ProjectInput {
+    //templateElement: HTMLTemplateElement;
+    //hostElement: HTMLTemplateElement;
+
+
+    constructor() {
+        //this.templateElement = document.getElementById('project-input')!;
+        //this.hostElement = document.getElementById('project-list')!;
+    }
+}
