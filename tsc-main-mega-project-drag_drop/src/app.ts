8e75e33e91c5a955917e6dff350129e10fcd7289
Shubham Sarkar <ronniecse8@gmail.com>
2023-09-10 15:31:40 +0530

    TYPRESCRIPT_PROJECT_DRAG_AND_DROP_03:31PM_IST_SEP_09_2023

--- tsc-main-mega-project-drag_drop/src/app.ts
+++ tsc-main-mega-project-drag_drop/src/app.ts
@@ -1,6 +1,23 @@
+//Project Type
+enum ProjectStatus {
+    Active,
+    Finished
+}
+
+class Project {
+    constructor(public id: string, public title: string, public description: string, public people: number, public status: ProjectStatus) {
+
+    }
+}
+
+
+// Project State Management
+type Listener = (items: Project[]) => void;
+
+
 class ProjectState {
-    private listners: any[] = [];
-    private projects: any[] = [];
+    private listners: Listener[] = [];
+    private projects: Project[] = [];
     private static instance: ProjectState;
 
     private constructor() {
@@ -15,19 +32,19 @@ class ProjectState {
         return this.instance;
     }
 
-    addListner(listnerFn: Function){
+    addListner(listnerFn: Listener){
         this.listners.push(listnerFn);
 
     }
 
     addProjects(title: string, description:string, numOfPeople: number){
-        const newProject = {
-            id: Math.random().toString(),
-            title: title,
-            desc:description,
-            people:numOfPeople
-
-        };
+        const newProject = new Project(Math.random().toString(), title, description,numOfPeople,ProjectStatus.Active);
+            // {
+            // id: Math.random().toString(),
+            // title: title,
+            // desc:description,
+            // people:numOfPeople
+            //    };
         this.projects.push(newProject);
         for(const listnerFN of this.listners){
             listnerFN(this.projects.slice());
@@ -94,7 +111,7 @@ class ProjectList {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLElement;
-    assignedProjects: any[];
+    assignedProjects: Project[];
 
     constructor(private type: 'active'|'finished') {
         this.templateElement = document.getElementById(
@@ -110,7 +127,7 @@ class ProjectList {
         this.element = importedNode.firstElementChild as HTMLElement;
         this.element.id = `${this.type}-projects`;
 
-        projectState.addListner((projects: any[]) => {
+        projectState.addListner((projects: Project[]) => {
             this.assignedProjects = projects;
             this.renderProjects();
         })
